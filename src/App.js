23cbcb6aabec53c7a15f7221625058293d0c06b0
Shreyas Mudakavi <shreyasmudak@gmail.com>
2022-09-28 21:15:29 +0530

    fetching news by source complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
-function App() {
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: "5c284b31f31d9328fc2ca2b63eedc90f2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewsCards articles={newsArticles} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewsCard = ({ article, i }) => {
+  return <div>NewsCard</div>;
+};
+
+export default NewsCard;
