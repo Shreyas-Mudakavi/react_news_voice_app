ef5b1fe80220a4d61605265089e94a9d8d87d79a
Shreyas Mudakavi <shreyasmudak@gmail.com>
2022-09-29 15:27:55 +0530

    fetching news by source complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
 
+import useStyles from "./styles.js";
 const App = () => {
+  const classes = useStyles();
+
   const [newsArticles, setNewsArticles] = useState([]);
 
   useEffect(() => {
@@ -18,6 +21,13 @@ const App = () => {
 
   return (
     <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://media.gettyimages.com/photos/digital-marketing-picture-id1222811180?k=20&m=1222811180&s=612x612&w=0&h=pbx0i1gHDYn1vgQlCaFcOHP2GW3GpB-LCSAhx7JUZwo="
+          alt=""
+          className={classes.alanLogo}
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,7 +1,63 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 
-const NewsCard = ({ article, i }) => {
-  return <div>NewsCard</div>;
+import useStyles from "./styles.js";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            "https://ou.ac.lk/wp-content/uploads/2021/02/News-2.jpg"
+          }
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textPrimary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default NewsCard;
